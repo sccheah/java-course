63ebc588b96da19c7f912b70c141670b2fc1faa6
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 01:59:48 -0700

    determine leap year by year input

--- mooc-2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
@@ -1,10 +1,28 @@
 
 import java.util.Scanner;
 
+/*
+A year is a leap year if it is divisible by 4. 
+But if the year is divisible by 100, it is a leap year 
+    only when it is also divisible by 400.
+
+Create a program that checks whether the given year is a leap year.
+*/
 public class LeapYear {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(reader.nextLine());
+        
+        if ((year % 100 == 0) && (year % 400 == 0))
+            System.out.println("The year is a leap year");
+        else if ((year % 100 == 0) && (year % 400 != 0))
+            System.out.println("The year is not a leap year");
+        else if (year % 4 == 0)
+            System.out.println("The year is a leap year");
+        else
+            System.out.println("The year is not a leap year");
     }
 }
