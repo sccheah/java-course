63065585dd6f947d0c26975d497f2e22a418d2a3
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 02:46:12 -0700

    Graph temperatures based on user inputs

--- mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
@@ -8,12 +8,30 @@ public class Temperatures {
         Scanner reader = new Scanner(System.in);
         // Write your code here. 
 
+        System.out.print("Type a number (or type \"quit\"): ");
+        String input = reader.nextLine();
+        Double number; 
+        
+        while (true)
+        {
+            if (input.equals("quit"))
+                break;
+            
+            number = Double.parseDouble(reader.nextLine());
+            // temp below -30 deg or over 40 deg are ignored
+            if ((number > -30.0) && (number < 40.0))
+                Graph.addNumber(number);
+            
+            System.out.print("Type a number (or type \"quit\"): ");
+            input = reader.nextLine();
+        }
+        
         // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
+//        Graph.addNumber(7);
+//        double value = 13.5;
+//        Graph.addNumber(value);
+//        value = 3;
+//        Graph.addNumber(value);
         // Remove or comment out these lines above before trying to run the tests.
     }
 }
