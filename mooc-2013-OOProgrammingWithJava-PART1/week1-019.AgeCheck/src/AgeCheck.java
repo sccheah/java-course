1dcdcf8ca26cd7936843f3d258e7bc2bb4f64688
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 01:26:00 -0700

    test user's age is at least 0 and at most 120

--- mooc-2013-OOProgrammingWithJava-PART1/week1-019.AgeCheck/src/AgeCheck.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-019.AgeCheck/src/AgeCheck.java
@@ -1,10 +1,19 @@
 
 import java.util.Scanner;
 
+// Create a program that asks for the user's age and checks that it is 
+//      reasonable (at least 0 and at most 120).
 public class AgeCheck {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(reader.nextLine());
+        
+        if (age >= 0 && age <= 120)
+            System.out.println("OK");
+        else
+            System.out.println("Impossible!");
     }
 }
