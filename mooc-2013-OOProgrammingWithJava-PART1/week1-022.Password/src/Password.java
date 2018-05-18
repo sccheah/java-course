a7ff1417b6cb32bd992c1f5f00c8856b60ea4d70
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 02:09:04 -0700

    while loop to constantly prompt user for password tries

--- mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -8,5 +8,17 @@ public class Password {
         String password = "carrot"; // Use carrot as password when running tests.
 
         // Write your code here
+        System.out.print("Type the password: ");
+        String passInput = reader.nextLine();
+        
+        while (!passInput.equals(password))
+        {
+            System.out.println("Wrong!");
+            System.out.print("Type the password: ");
+            passInput = reader.nextLine();
+        }
+        
+        System.out.println("Right!\n");
+        System.out.println("The secret is: jryy qbar!");
     }
 }
