146259ee28d1b53dd6dd6cfc59ea837b32f99f34
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 01:20:29 -0700

    edit out logical operators to just rely on control flow

--- mooc-2013-OOProgrammingWithJava-PART1/week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -19,15 +19,15 @@ public class GradesAndPoints {
         System.out.print("Type the points [0-60]: ");
         int grade = Integer.parseInt(reader.nextLine());
         
-        if (grade >= 0 && grade < 30)
+        if (grade < 30)
             System.out.println("Grade: failed");
-        else if (grade >= 30 && grade < 35)
+        else if (grade < 35)
             System.out.println("Grade: 1");
-        else if (grade >= 35 && grade < 40)
+        else if (grade < 40)
             System.out.println("Grade: 2");
-        else if (grade >= 40 && grade < 45)
+        else if (grade < 45)
             System.out.println("Grade: 3");
-        else if (grade >= 45 && grade < 50)
+        else if (grade < 50)
             System.out.println("Grade: 4");
         else
             System.out.println("Grade: 5");
