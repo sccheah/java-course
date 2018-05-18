bdb16626d1f7ce043e5a2cf1d5db648b9e34fa28
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 00:19:05 -0700

    perform various operations on NHLStatistics package

--- mooc-2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
@@ -4,5 +4,31 @@ import nhlstats.NHLStatistics;
 public class Main {
 
     public static void main(String[] args) {
+        
+        // prints top ten players based on goals
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+        
+        System.out.println("");
+        
+        // prints top 25 players based on penalty amounts
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+        
+        System.out.println("");
+        
+        // prints statistics for Sidney Crosby
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+        
+        System.out.println("");
+        
+        // prints stats for Philadelphia Flyers
+        NHLStatistics.teamStatistics("PHI");
+        
+        System.out.println("");
+        
+        // prints the players in Anaheim Ducks ordered by points
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
     }
 }
