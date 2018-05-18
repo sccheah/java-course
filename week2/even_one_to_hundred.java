acee11c8bc46c45731daa7418a64d2a11d93be3a
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 13:25:05 -0700

    print even num from 2 to 100 in while loop

---
+++ week2/even_one_to_hundred.java
@@ -0,0 +1,15 @@
+// Create a program that prints all even numbers between 2 and 100.
+
+public class even_one_to_hundred
+{
+    public static void main(String[] args)
+    {
+        int ctr = 2;
+
+        while (ctr <= 100)
+        {
+            System.out.println(ctr);
+            ctr += 2;
+        }
+    }
+}
