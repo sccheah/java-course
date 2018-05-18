d6846e22a1209d836eea53fb9f7b2dd05a569404
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-17 23:25:09 -0700

    complete division of two inputs

--- mooc-2013-OOProgrammingWithJava-PART1/week1-009.Divider/src/Divider.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-009.Divider/src/Divider.java
@@ -7,5 +7,11 @@ public class Divider {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user.
+        System.out.print("Type a number: ");
+        double num1 = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        double num2 = Integer.parseInt(reader.nextLine());
+        
+        System.out.println("Division: " + num1 + " / " + num2 + " = " + (num1 / num2));
     }
 }
