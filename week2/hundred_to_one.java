ccf85a436ca5a37e83fd3607df39af5762574c78
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 13:19:59 -0700

    print hundred to one with while loop

---
+++ week2/hundred_to_one.java
@@ -0,0 +1,13 @@
+public class hundred_to_one
+{
+    public static void main(String [] args)
+    {
+        int ctr = 100;
+
+        while (ctr > 0)
+        {
+            System.out.println(ctr);
+            ctr--;
+        }
+    }
+}
