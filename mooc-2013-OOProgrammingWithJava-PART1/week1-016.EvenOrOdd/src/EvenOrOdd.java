51a8bd1c17b4a07a53801109089bb69891c5f68a
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-18 00:40:36 -0700

    test if int input is even or odd

--- mooc-2013-OOProgrammingWithJava-PART1/week1-016.EvenOrOdd/src/EvenOrOdd.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-016.EvenOrOdd/src/EvenOrOdd.java
@@ -7,7 +7,13 @@ public class EvenOrOdd {
         Scanner reader = new Scanner(System.in);
 
         // Type your program here
-
+        System.out.println("Type a number: ");
+        int num = Integer.parseInt(reader.nextLine());
+        
+        if (num % 2 == 0)
+            System.out.println("Number " + num + " is even.");
+        else
+            System.out.println("Number " + num + " is odd.");
         // NOTE:
         // Parity of a value can be easily checked with modulo-operator %.
         // Test the following:
