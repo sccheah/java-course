2368470a3c0ffbb946e3fb58cf3d9e64437d4752
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-19 04:34:24 -0700

    use method to print a string

---
+++ week2/print_text.java
@@ -0,0 +1,14 @@
+// use a method to print a string
+
+public class print_text
+{
+    public static void main(String[] args)
+    {
+        printText();
+    }
+
+    public static void printText()
+    {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+}
