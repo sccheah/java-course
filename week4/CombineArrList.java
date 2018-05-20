a65d256add8b1fe2937ae218e203ef75eebc341a
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-20 04:49:22 -0700

    concatenate a list with another excluding elements already existing in the other list

--- week4/CombineArrList.java
+++ week4/CombineArrList.java
@@ -15,6 +15,16 @@ public class CombineArrList
         l1.addAll(l2);
     }
 
+    // concatenate nums from l2 that do not exist in l1 already
+    public static void smart_combine(ArrayList<Integer> l1, ArrayList<Integer> l2)
+    {
+        for (Integer num : l2)
+        {
+            if (!l1.contains(num))
+                l1.add(num);
+        }
+    }
+
     public static void main(String[] args)
     {
         ArrayList<Integer> list1 = new ArrayList<Integer> ();
@@ -22,12 +32,14 @@ public class CombineArrList
 
         list1.add(4);
         list1.add(3);
+        list1.add(5);
 
         list2.add(5);
         list2.add(10);
         list2.add(7);
 
-        combine(list1, list2);
+        //combine(list1, list2);
+        smart_combine(list1, list2);
 
         System.out.println(list1);  // [4, 3, 5, 10, 7]
         System.out.println(list2);  // [5, 10, 7]
