59e4b94216a2a0486cef8cd43df4931d0e0bce8b
Stanley Cheah <sccheah@ucdavis.edu>
2018-05-23 11:54:06 -0700

    include setter func and BMI calculator

--- week4/Main.java
+++ week4/Main.java
@@ -13,17 +13,48 @@ public class Main
 
         System.out.println();
 
-        bob.become_older();
-        bob.become_older();
+        for (int i = 0; i < 30; i++)
+            bob.become_older();
+
         martin.become_older();
 
-        bob.print_person();
-        martin.print_person();
-        brian.print_person();
+        System.out.println(bob);
+        System.out.println(martin);
+        System.out.println(brian);
+
+        System.out.println();
+
+        if ( martin.is_adult() ) {
+            System.out.println(martin.get_name() + " is an adult");
+        } else {
+            System.out.println(martin.get_name() + " is an adult");
+        }
+
+        if ( bob.is_adult() ) {
+            System.out.println(bob.get_name() + " is an adult");
+        } else {
+            System.out.println(bob.get_name() + " is an minor");
+        }
 
         System.out.println();
         //////// Product
         Product product = new Product("Banana", 1.1, 13);
         product.print_product();
+
+        /////////////////////////
+        System.out.println();
+        System.out.println();
+
+        Person matti = new Person("Matti");
+        Person john = new Person("John");
+
+        matti.setHeight(180);
+        matti.setWeight(86);
+
+        john.setHeight(175);
+        john.setWeight(64);
+
+        System.out.println(matti.get_name() + ", body mass index: " + String.format("%.2f", matti.bodyMassIndex()));
+        System.out.println(john.get_name() + ", body mass index: " + String.format("%.2f", john.bodyMassIndex()));
     }
 }
--- week4/Person.java
+++ week4/Person.java
@@ -2,11 +2,15 @@ public class Person
 {
     private String name;
     private int age;
+    private int height;
+    private int weight;
 
     public Person(String initialName)
     {
         this.age = 0;
         this.name = initialName;
+        this.weight = 0;
+        this.height = 0;
     }
 
     public void print_person()
@@ -17,5 +21,37 @@ public class Person
     public void become_older()
     {
         this.age++;
+    }
+
+    public boolean is_adult()
+    {
+        return this.age >= 18;
+    }
+
+    public String get_name()
+    {
+        return this.name;
+    }
+
+    public void setHeight(int newHeight)
+    {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight)
+    {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex()
+    {
+        double heightDividedByHundred = this.height / 100.0;
+        return this.weight / (heightDividedByHundred * heightDividedByHundred);
+    }
+
+    // with this, we can just call something like System.out.println(bob);
+    public String toString()
+    {
+        return this.name + ", age " + this.age + " years";
     }
 }
