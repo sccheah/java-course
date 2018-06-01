96072021bec6aa1a5c11c452f53d75b7f1a82c3c
Stanley Cheah <sccheah@ucdavis.edu>
2018-06-01 04:55:58 -0700

    implement search with String contains()

--- week6/Library.java
+++ week6/Library.java
@@ -22,19 +22,62 @@ public class Library
             System.out.println(book);
     }
 
+    public ArrayList<Book> searchByTitle(String title)
+    {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book : books)
+            if (book.getTitle().contains(title))
+                found.add(book);
+
+        return found;
+    }
+
+    public ArrayList<Book> searchByPublisher(String publisher)
+    {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book : books)
+            if (book.getPublisher().contains(publisher))
+                found.add(book);
+
+        return found;
+    }
+
+    public ArrayList<Book> searchByYear(int year)
+    {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book : books)
+            if (book.getYear() == year)
+                found.add(book);
+
+        return found;
+    }
+
     public static void main(String[] args)
     {
         Library Library = new Library();
 
-        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
-        Library.addBook(cheese);
-
-        Book nhl = new Book("NHL Hockey", "Stanley Kupp", 1952);
-        Library.addBook(nhl);
-
+        Library.addBook(new Book("Cheese Problems Solved", "Woodhead Publishing", 2007));
+        Library.addBook(new Book("The Stinky Cheese Man and Other Fairly Stupid Tales", "Penguin Group", 1992));
+        Library.addBook(new Book("NHL Hockey", "Stanley Kupp", 1952));
         Library.addBook(new Book("Battle Axes", "Tom A. Hawk", 1851));
 
-        Library.printBooks();
+        ArrayList<Book> result = Library.searchByTitle("Cheese");
+        for (Book book: result) {
+            System.out.println(book);
+        }
+
+        System.out.println("---");
+        for (Book book: Library.searchByPublisher("Penguin Group  ")) {
+            System.out.println(book);
+        }
+
+        System.out.println("---");
+        for (Book book: Library.searchByYear(1851)) {
+            System.out.println(book);
+        }
     }
 
 }
