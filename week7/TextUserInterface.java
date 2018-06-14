2fdba0c0077b11caaf4c72b9f52dcd2fa45a4c64
Stanley Cheah <sccheah@ucdavis.edu>
2018-06-13 23:15:26 -0700

    include text user interface to add and translate to/from dictionary

---
+++ week7/TextUserInterface.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class TextUserInterface {
+    private Scanner scanner;
+    private Dictionary dictionary;
+
+    public TextUserInterface(Scanner reader, Dictionary dictionary) {
+        scanner = reader;
+        this.dictionary = dictionary;
+    }
+
+    private void printMenu() {
+        System.out.println("Statements:");
+        System.out.println("    add - adds a word pair to the dictionary");
+        System.out.println("    translate - asks a word and prints its translation");
+        System.out.println("    quit - quits the text user interface");
+    }
+
+    public void start() {
+        printMenu();
+
+        System.out.print("\nStatement: ");
+        String statement = scanner.nextLine();
+        statement.toLowerCase();
+        statement = statement.trim();
+
+        String word, translation;
+
+        while (!statement.equals("quit")) {
+            if (statement.equals("add")) {
+                System.out.print("In Finnish: ");
+                word = scanner.nextLine();
+                
+                System.out.print("Translation: ");
+                translation = scanner.nextLine();
+
+                dictionary.add(word, translation);
+            }
+
+            if (statement.equals("translate")) {
+                System.out.print("Give a word: ");
+                word = scanner.nextLine();
+
+                System.out.println("Translation: " + dictionary.translate(word));
+            }
+
+            System.out.print("\nStatement: ");
+            statement = scanner.nextLine();
+        }
+
+        System.out.println("Cheers!");
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Dictionary dictionary = new Dictionary();
+
+        TextUserInterface UI = new TextUserInterface(reader, dictionary);
+        UI.start();
+    }
+}
